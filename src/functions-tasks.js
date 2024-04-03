066a293a14833a0809535a0418a77a33221a3a86
natagetup <n.kulikouskaya@gmail.com>
2024-04-03 18:17:01 +0200

    feat: implement getIdGeneratorFunction task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -225,8 +225,13 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let counter = startFrom;
+  return function generateId() {
+    const currentId = counter;
+    counter += 1;
+    return currentId;
+  };
 }
 
 module.exports = {
